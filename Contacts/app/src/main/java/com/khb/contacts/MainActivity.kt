b3e7fd0cd7db41a8f067ea22203e9e86c5f02ee2
kimhyebeen <kimhbin@naver.com>
2020-09-07 16:01:38 +0900

    [view] setting Activity

--- Contacts/app/src/main/java/com/khb/contacts/MainActivity.kt
+++ Contacts/app/src/main/java/com/khb/contacts/MainActivity.kt
@@ -2,10 +2,25 @@ package com.khb.contacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.khb.contacts.database.ContactEntity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var contactViewModel: ContactViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * 뷰모델 객체는 직접적으로 초기화 해주는 것이 아니라, 안드로이드 시스템을 통해 생성해준다.
+         * 시스템에서는 만약 이미 생성된 ViewModel 인스턴스가 있다면 이를 반환할 것이므로 메모리 낭비를 줄여준다.
+         * 따라서 ViewModelProviders를 이용해 get 해준다.
+         */
+        contactViewModel = ViewModelProviders.of(this).get(ContactViewModel::class.java)
+        contactViewModel.getAll().observe(this, Observer<List<ContactEntity>> {
+            // update UI
+        })
     }
 }
