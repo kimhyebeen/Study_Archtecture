5ac0f57d6a2cc1db2e2346548de8c7c2038e6690
kimhyebeen <kimhbin@naver.com>
2020-09-08 11:16:18 +0900

    update files

--- Contacts/app/src/main/java/com/khb/contacts/database/ContactDatabase.kt
+++ Contacts/app/src/main/java/com/khb/contacts/database/ContactDatabase.kt
@@ -1,13 +1,16 @@
 package com.khb.contacts.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [ContactEntity::class], version = 1)
 abstract class ContactDatabase: RoomDatabase() {
     abstract fun contactDao(): ContactDao
 
     companion object {
+        @Volatile
         private var INSTANCE: ContactDatabase? = null
 
         fun getInstance(context: Context): ContactDatabase? {
@@ -19,8 +22,8 @@ abstract class ContactDatabase: RoomDatabase() {
                 INSTANCE = Room.databaseBuilder(context.applicationContext, ContactDatabase::class.java, "contact")
                     .fallbackToDestructiveMigration() // 데이터베이스가 갱신될 때 기존의 테이블을 버리고 새로 사용
                     .build()
+                return INSTANCE
             }
-            return INSTANCE
         }
     }
 }
