9f0e08c99d68776c057181ffcd522ed0d39bb3c2
kimhyebeen <kimhbin@naver.com>
2020-12-30 02:26:23 +0900

    [Issue-9] Create a iOS Project for MVVM Pattern.

---
+++ iOS/Dictionary01/Dictionary01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dictionary01
+//
+//  Created by 김혜빈 on 2020/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
