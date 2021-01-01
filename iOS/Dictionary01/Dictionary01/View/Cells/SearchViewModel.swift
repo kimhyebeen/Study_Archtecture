dd026729fa187f2068c78a853e2a7bc97a2a8028
kimhyebeen <kimhbin@naver.com>
2021-01-01 20:32:17 +0900

    [Issue-10] ViewModel 구현 및 백과사전 API 통신 구현

--- iOS/Dictionary01/Dictionary01/View/Cells/SearchViewModel.swift
+++ iOS/Dictionary01/Dictionary01/View/Cells/SearchViewModel.swift
@@ -6,13 +6,39 @@
 //
 
 import Foundation
+import Alamofire
 
 class SearchViewModel {
+    let baseUrl = "https://openapi.naver.com/v1/search/encyc.json"
     var dictionaryItems: Observable<[DictionaryModel]?> = Observable(nil)
     
     func requestSearch(word: String?) {
         guard let searchWord: String = word, !(word?.isEmpty ?? true) else {
             return
+        }
+        
+        AF.request(
+            baseUrl,
+            method: .get,
+            parameters: ["query":searchWord],
+            headers: ["X-Naver-Client-Id":"Z6OHzyRe_5anc4FWCoq2", "X-Naver-Client-Secret":"pSumWoEXFY"]
+        ).responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    self.getDictionaryModel(value: value)
+                case .failure(let error):
+                    print("SearchViewModel - requestSearch - error: \(error.localizedDescription)")
+                }
+            }
+    }
+    
+    func getDictionaryModel(value: Any){
+        do {
+            let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+            let dictionaryResponse: DictionaryResponse = try JSONDecoder().decode(DictionaryResponse.self, from: data)
+            self.dictionaryItems.value = dictionaryResponse.items
+        } catch {
+            print(error.localizedDescription)
         }
     }
     
