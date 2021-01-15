7bc0fea68d130ce8a88cbf36d06385975202e784
kimhyebeen <kimhbin@naver.com>
2021-01-15 17:20:32 +0900

    [Issue-13] ViewModel 생성

--- iOS/SignIn/SignIn/SignInVC.swift
+++ iOS/SignIn/SignIn/SignInVC.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SwiftUI
 
 class SignInVC: UIViewController {
     let roundedRectangle = SignInRoundedRectangle()
@@ -23,25 +22,3 @@ class SignInVC: UIViewController {
     }
 
 }
-
-#if DEBUG
-import SwiftUI
-struct ViewControllerRepresentabletable: UIViewControllerRepresentable {
-    // update
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-
-    }
-
-    // make UI
-    @available(iOS 13.0, *)
-    func makeUIViewController(context: Context) -> some UIViewController {
-        SignInVC()
-    }
-}
-
-struct ViewController_Previews: PreviewProvider {
-    static var previews: some View {
-        ViewControllerRepresentabletable().previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
-    }
-}
-#endif
---
+++ iOS/SignIn/SignIn/SignInViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewModel.swift
+//  SignIn
+//
+//  Created by 김혜빈 on 2021/01/15.
+//
+
+class SignInViewModel {
+    let input = Input()
+    let output = Output()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+}
