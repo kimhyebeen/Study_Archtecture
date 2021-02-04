8b7a29e275b36cfdbedb3d0022114b78ce31f326
kimhyebeen <kimhbin@naver.com>
2021-02-05 02:29:30 +0900

    #22. CoreData - entity 생성

---
+++ iOS/SearchImage/SearchImage/ImageData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ImageData+CoreDataClass.swift
+//  
+//
+//  Created by 김혜빈 on 2021/02/05.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ImageData)
+public class ImageData: NSManagedObject {
+
+}
