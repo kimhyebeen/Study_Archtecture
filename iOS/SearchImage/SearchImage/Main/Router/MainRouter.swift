a5482d500f8da0284abe4f530203929946766b97
kimhyebeen <kimhbin@naver.com>
2021-02-05 01:45:15 +0900

    #20. Main - MainRouter 구현

---
+++ iOS/SearchImage/SearchImage/Main/Router/MainRouter.swift
@@ -0,0 +1,31 @@
+//
+//  MainRouter.swift
+//  SearchImage
+//
+//  Created by 김혜빈 on 2021/02/05.
+//
+
+import UIKit
+
+class MainRouter: MainRouterProtocol {
+    static func createModule() -> MainViewController {
+        let view = MainViewController()
+        
+        let presenter: MainViewToPresenterProtocol & MainInteractorToPresenterProtocol = MainPresenter()
+        let interactor: MainInteractorProtocol = MainInteractor()
+        let router: MainRouterProtocol = MainRouter()
+        
+        view.presentor = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        
+        return view
+    }
+    
+    func pushToDetailImageScreen(navigationConroller: UINavigationController) {
+        let detailImageModue = DetailImageRouter.createModule()
+        navigationController.pushViewController(detailImageModue, animated: true)
+    }
+}
