74c254f448886f0b8aca253b43c6ce11b9fef941
kimhyebeen <kimhbin@naver.com>
2021-02-07 00:39:59 +0900

    #19. Detail - ViewController 생성

---
+++ iOS/SearchImage/SearchImage/Detail/View/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  SearchImage
+//
+//  Created by 김혜빈 on 2021/02/07.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    func setupView() {
+        setupScrollView()
+        setupImageView()
+    }
+
+}
+
+// MARK: +UI
+extension DetailViewController {
+    // MARK: Scroll View
+    func setupScrollView() {
+        
+    }
+    
+    // MARK: Image View
+    func setupImageView() {
+        
+    }
+}
