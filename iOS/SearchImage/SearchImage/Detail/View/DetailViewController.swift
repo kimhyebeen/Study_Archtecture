80a34e751bc0f7db0493bf6bcaa4bdb2fb3037ff
kimhyebeen <kimhbin@naver.com>
2021-02-07 00:49:30 +0900

    #19. Detail - ImageView 추가

--- iOS/SearchImage/SearchImage/Detail/View/DetailViewController.swift
+++ iOS/SearchImage/SearchImage/Detail/View/DetailViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class DetailViewController: UIViewController {
     var scrollView: UIScrollView!
+    var imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +29,9 @@ extension DetailViewController {
     // MARK: Scroll View
     func setupScrollView() {
         scrollView = UIScrollView(frame: self.view.bounds)
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.minimumZoomScale = 0.5
+        scrollView.maximumZoomScale = 3.0
         self.view.addSubview(scrollView)
         
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -39,6 +43,13 @@ extension DetailViewController {
     
     // MARK: Image View
     func setupImageView() {
+        imageView.contentMode = .scaleAspectFit
+        self.view.addSubview(imageView)
         
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
     }
 }
